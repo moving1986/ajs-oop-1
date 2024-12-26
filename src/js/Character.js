6df1e826095b58ce69400e0c7c78bce332a831d8
Aleksei Mezentsev <aleksei_mezencev@mail.ru>
2024-12-26 13:09:10 +0700

    Init

---
+++ src/js/Character.js
@@ -0,0 +1,17 @@
+class Character {
+    constructor(name, type, attack, defence) {
+        this.name = name;
+        this.type = type;
+        health = 100;
+        level = 1;
+
+
+
+    }
+
+    checkName() {
+        
+    }
+
+
+}
